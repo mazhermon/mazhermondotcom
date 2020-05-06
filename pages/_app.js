7fcbc779c21f0b1f6eb4745e901e5fad2667f63e
Maz Hermon <mazhermon@gmail.com>
2020-05-07 09:18:39 +1200

    update waves colour and add new colours to theme

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,25 @@ import GlobalStyles from '../styles/globalStyles';
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: '#8C7EBF',
+    primaryDark: '#464A8C',
+
+    secondary: '#3061F2',
+    secondaryLight: '#7292F2',
+    secondaryDark: '#134BF2',
+
+    accent1: '#F24150',
+    accent2: '#F2A7B5',
+    accent3: '#FDF9BC',
+
+    bgLight: '#BFDCDF',
+    bgSuperLight: '#EBFFFB',
+
+    gray1: '#F8F8FD',
+    gray2: '#B0B0BF',
+    gray3: '#A0A0BC',
+    gray: '#8C7EBF',
+
     bgDarkMode: '#332e2e',
     textDarkMode: '#ece8e8',
   },
